11a9a15b8143e221cdedf158b1f6288a47aa11c6
Durham DJ Society <51788616+DurhamDJ@users.noreply.github.com>
2019-08-27 21:41:43 +0100

    Update DJs.js

--- DJs.js
+++ DJs.js
@@ -49,7 +49,7 @@ let djs = {
             'drum and bass'        
         ],
         'music': [
-            ['soundcloud', '662385164']
+            ['soundcloud', '662385164'],
             ['soundcloud', '668394668']
         ]
     },      
