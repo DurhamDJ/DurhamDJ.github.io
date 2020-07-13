2dabea66e2627be51f7dcaca591afeab5462d002
Barnaby Collins <barnabyc@sky.com>
2020-07-13 16:31:38 +0100

    Properly fix widget scaling

--- widgets/scale-widgets.js
+++ widgets/scale-widgets.js
@@ -1,13 +1,15 @@
 function checkScales() {
     $('.demo-widget').map((_, parent) => {
-        let height = 0;
+        let height = 0, width = 0;
 
         $(parent).children('iframe').map((_, frame) => {
-            $(frame).css('transform', `scale(${Math.min($(parent).width()/$(frame).width(), 1.0)})`);
+            $(frame).css('transform', `scale(${Math.min($(parent).parent().width()/$(frame).width(), 0.5)})`);
             height += frame.getBoundingClientRect().height;
+            width = Math.max(width, frame.getBoundingClientRect(). width);
         });
         
         $(parent).height(height);
+        $(parent).width(width);
     });
 }
 window.onresize = checkScales;
