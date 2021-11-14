977585608b71e2252a277758dbf39c78e124b702
Barnaby Collins <barnabyc@sky.com>
2021-11-14 18:47:02 +0000

    Fix my Mixcloud link

--- DJs.js
+++ DJs.js
@@ -94,7 +94,7 @@ let djs = {
         ],
         'music': [
             ['youtube', 't_-JCmJ9pEc', '33'],
-            ['youtube', '/barnabycollins/lockdown-livestreams-2-entering-the-cool-zone-with-barnaby-and-dominic/'],
+            ['mixcloud', '/barnabycollins/lockdown-livestreams-2-entering-the-cool-zone-with-barnaby-and-dominic/'],
             ['mixcloud', '/barnabycollins/jazzy-drum-bass-mix-1/']
         ],
         'socials': {
