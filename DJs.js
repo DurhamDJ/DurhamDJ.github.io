d256bee907e2555d82be87e879a0176de8ffa60f
Durham DJ Society <51788616+DurhamDJ@users.noreply.github.com>
2022-09-20 13:55:54 +0100

    Update DJs.js

--- DJs.js
+++ DJs.js
@@ -1,110 +1,5 @@
 let djs = {
-    'Aidan McKenzie': {
-        'bio': 'Aidan McKenzie [formerly deadW] is a musician, producer, and a very experienced \
-                DJ with a DJ residency at Klute! With appearances at summer balls, bops, \
-                freshers events, and house parties across Durham he has a range of styles which \
-                he loves playing with - there\'s a fun, cheesey, and dancey side to get everyone \
-                on their feet, jumping in the air and singing along, and he\'s very comfortable \
-                playing with the vibe and spinning in some of his edgier side fluently. Whatever \
-                the event, expect fresh new, bootlegs, mashups, and remixes (many of which he \
-                would have produced himself) - be prepared to hear classics, bangers, and the \
-                Durham cheese like you have never heard them before!',
-        'img': 'assets/djimg/aidan.jpg',
-        'genres': [
-            'pop',
-            'dance',
-            'EDM',
-            'psy-trance',
-            'hardcore'
-        ],
-        'music': [
-            ['youtube', 'LBnAGz0TIIw', '480']
-        ],
-        'socials': {
-            'facebook': 'aidsmckenzie',
-            'soundcloud': 'aidsmckenzie',
-            'mixcloud': 'aidsmckenzie'
-        }
-    },
-    
-    'Laszlo Karsai': {
-        'bio': 'Laszlo Karsai aka Laszlo has been making the Durham DJ scene his home \
-                since late 2018. Promising to bring energy and groove to every occasion, \
-                his sets fuse fresh selections of house music effortlessly.',
-        'img': 'assets/djimg/Laszlo.jpg',
-        'genres': [
-            'commercial dance',
-            'house',
-            'tech house',
-            'disco',
-            'funk'
-        ],
-        'music': [
-            ['mixcloud', '/LO_LA/the-sound-of-swing/'],
-            ['mixcloud', '/LO_LA/journey-1-from-house-to-techno/']
-        ],
-        'socials': {
-        }
-    },
-    
-    'Barnaby Collins': {
-        'bio': 'Barnaby\'s reputation precedes him in the Durham DJ scene thanks to his clean mixes, \
-                high energy and varied music selection. In 2020, he could often be found streaming sets on a \
-                number of different channels, but as Durham reopens in 2021-22 he is keen to resume bringing his \
-                signature creativity, passion and experimentalism to nights of all sizes.',
-        'img': 'assets/djimg/Barnaby.jpg',
-        'genres': [
-            'drum and bass',
-            'ambient'
-        ],
-        'music': [
-            ['youtube', 't_-JCmJ9pEc', '33'],
-            ['mixcloud', '/barnabycollins/fractal-iii-tesselate-barnaby-collins/'],
-            ['mixcloud', '/barnabycollins/lockdown-livestreams-2-entering-the-cool-zone-with-barnaby-and-dominic/'],
-            ['mixcloud', '/barnabycollins/jazzy-drum-bass-mix-1/']
-        ],
-        'socials': {
-            'facebook': 'barnaby.a.collins',
-            'mixcloud': 'barnabycollins',
-            'soundcloud': 'barnabycollins'
-        }
-    },
-
-    'Jake Roberts': {
-        'bio': 'Jake has been DJ\'ing as jakeverse for around 6 years, first privately, \
-                then as a new, regular member of the DDJS at the weekly open decks. His music \
-                selection and style of mixing is unique, comfortably mixing anything from groovy \
-                tech house to hard hitting techno. Jake also produces the odd tech house tune, \
-                which he has no problem playing in front of a crowd.',
-        'img': 'assets/djimg/Jake.jpg',
-        'genres': [
-            'tech house',
-            'techno'
-        ],
-        'music': [
-            ['spotify-track', '2eKGGJludrq2pww8soAJui'],
-            ['mixcloud', '/JakeVerse/tech-housetechno-mix-128-145bpm-260620/']
-        ],
-        'socials': {
-            'facebook': 'jakeverseuk',
-            'spotify': '3KqWcHOLAgzotpuBKKz2un'
-        }
-    },
-
-    'Merry \'QVV\' Crossland': {
-        'bio': 'in between surviving zombie outbreaks and vampires, mer showcases a variety of techno music; \
-                from wonky acid bleep bloops, to banging bassy donks. all with a side of grimes and as many \
-                female/enby/trans electronic artists they come across.',
-        'img': 'assets/djimg/Merry.jpg',
-        'genres': [
-            'techno',
-            'dance'
-        ],
-        'socials': {
-            'facebook': 'merry.qvv',
-            'instagram': 'mer.aerqvv'
-        }
-    },
+    {
 
     'Charles Pritchard': {
         'bio': 'Charles first came onto the Durham scene with a clubby house mix for Rotate, and has more \
@@ -152,5 +47,18 @@ let djs = {
         ],
         'socials': {
         }
-    }
+    },
+    'Tash Quinn \'Missy\'':{
+        'bio' : 'Tash lives happily in the 170 realm surrounded by beautiful liquid and heavy jungle (and bad jokes). She\' \
+            a purist for drum and bass and you won\'t catch her mixing anything else. The self proclaimed lovechild of LSB and \
+            Alex Perez blends soulful and dark liquid,  minimal rollers - and a noice bit of jungle.',
+        'img': 'assets/djimg/Tash.jpg',
+        'genres' : [
+            'drum and bass', 
+            'jungle']
+        'music' : {
+        }
+        'socials' : {
+        }
+            
 };
