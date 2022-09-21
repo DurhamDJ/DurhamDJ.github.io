bed225f77f223b9ad2d9b529f44c04a19715b59e
Durham DJ Society <51788616+DurhamDJ@users.noreply.github.com>
2022-09-21 21:32:10 +0100

    Update DJs.js

--- DJs.js
+++ DJs.js
@@ -72,7 +72,7 @@ let djs = {
             'house'],
         'music' : [
              ],
-        'socials' : {'soundcloud': 'https://on.soundcloud.com/ibftT'
+        'socials' : {'soundcloud': '/tm-media?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing'
          }
     }
         
